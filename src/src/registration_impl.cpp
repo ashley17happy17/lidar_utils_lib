76c92784561ddcaf28b2c7c515bc6af656db91d8
ashley <ashley.yt.chiu@foxconn.com>
2026-06-30 17:13:56 +0800

    refactor: enable and format warning logs for ICP and NDT registration failures

--- src/src/registration_impl.cpp
+++ src/src/registration_impl.cpp
@@ -133,11 +133,10 @@ double executeGICP(CloudType::Ptr &cloud, CloudType::Ptr &map,
   if (result.num_inliers > 100 && out_fitness_score < config.score_threshold) {
     return out_fitness_score;
   } else {
-    // std::cerr
-    //     << "[WARN] Registration WARNING: failed to align, out_fitness_score:
-    //     "
-    //     << out_fitness_score << ", num_inliers: " << result.num_inliers
-    //     << std::endl;
+    std::cerr << "[WARN] ICP Failure: out_fitness_score:
+                 "
+              << out_fitness_score << ", num_inliers: " << result.num_inliers
+              << std::endl;
     return -1.0;
   }
 }
@@ -164,11 +163,10 @@ double executeNDT(CloudType::Ptr &cloud, CloudType::Ptr &map,
   if (ndt.hasConverged() && ndt.getFitnessScore() < config.score_threshold) {
     return ndt.getFitnessScore();
   } else {
-    // std::cerr
-    //     << "[WARN] Registration WARNING: failed to align, out_fitness_score:
-    //     "
-    //     << ndt.getFitnessScore() << ", hasConverged: " << ndt.hasConverged()
-    //     << std::endl;
+    std::cerr << "[WARN] NDT Failure: out_fitness_score:
+                 "
+              << ndt.getFitnessScore()
+              << ", hasConverged: " << ndt.hasConverged() << std::endl;
     return -1.0;
   }
 }
